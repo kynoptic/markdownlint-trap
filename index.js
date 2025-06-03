65a18d06fe0f5cff9237b221435b0356491ea2f1
kynoptic <kynoptic@noreply.codeberg.org>
2025-06-03 01:11:19 -0400

    chore: rename package from markdownlint-clarity to markdownlint-trap
    - Update package name in package.json
    - Update module documentation in index.js
    - Fix example paths in documentation to use new package name
    - Maintain consistent formatting in index.js module exports

--- index.js
+++ index.js
@@ -3,29 +3,29 @@
 "use strict";
 
 /**
- * Main entry point for markdownlint-clarity
- * 
+ * Main entry point for markdownlint-trap
+ *
  * @description Exports all custom markdownlint rules from this package as an array.
  * This module can be directly imported in markdownlint configurations to enable
  * all the custom rules provided by this package.
- * 
- * @module markdownlint-clarity
- * 
+ *
+ * @module markdownlint-trap
+ *
  * @example <caption>Using in a JavaScript-based markdownlint configuration</caption>
  * // In your .markdownlint.js file
  * const customRules = require("markdownlint-rules");
- * 
+ *
  * module.exports = {
  *   "default": true,
  *   "customRules": customRules,
  *   "sentence-case-headings-bold": true,
  *   "backtick-code-elements": true
  * };
- * 
+ *
  * @example <caption>Using with markdownlint API</caption>
  * const markdownlint = require("markdownlint");
  * const customRules = require("markdownlint-rules");
- * 
+ *
  * const options = {
  *   files: ["README.md"],
  *   customRules: customRules,
@@ -35,17 +35,17 @@
  *     "backtick-code-elements": true
  *   }
  * };
- * 
+ *
  * const result = markdownlint.sync(options);
  * console.log(result.toString());
- * 
+ *
  * @example <caption>Using with markdownlint-cli</caption>
  * // In your .markdownlint.json file
  * {
  *   "default": true,
  *   "customRules": [
- *     "./node_modules/markdownlint-clarity/rules/sentence-case.js",
- *     "./node_modules/markdownlint-clarity/rules/backtick-code-elements.js"
+ *     "./node_modules/markdownlint-trap/rules/sentence-case.js",
+ *     "./node_modules/markdownlint-trap/rules/backtick-code-elements.js"
  *   ],
  *   "sentence-case-headings-bold": true,
  *   "backtick-code-elements": true
@@ -55,7 +55,4 @@
 const backtickCodeElements = require("./rules/backtick-code-elements.js");
 const sentenceCase = require("./rules/sentence-case.js");
 
-module.exports = [
-  backtickCodeElements,
-  sentenceCase
-];
+module.exports = [backtickCodeElements, sentenceCase];
