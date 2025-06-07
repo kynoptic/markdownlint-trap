b108e13f098818633b1f632fb357bb8174cb687b
kynoptic <50030280+kynoptic@users.noreply.github.com>
2025-06-07 01:16:29 -0400

    docs: add missing jsdoc (#37)

--- src/logger.js
+++ src/logger.js
@@ -1,6 +1,9 @@
 import createDebug from 'debug';
 
-// Base debug instance for the project
+/**
+ * Base debug instance for the project.
+ * Enable by setting `DEBUG=markdownlint-trap*`.
+ */
 const debug = createDebug('markdownlint-trap');
 
 export default debug;
